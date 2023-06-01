7d4eda30b87f8f61dfb5d3f7ee9aa27d89f79ed1
Jake Holmquist <101203373+cloud-jake@users.noreply.github.com>
2023-06-01 09:37:57 -0400

    curl command needs OAUTH troubleshooting... 🐞

--- 03-create-bq.sh
+++ 03-create-bq.sh
@@ -7,7 +7,11 @@ SA_FULL="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 gcloud config set project $PROJECT_ID
 
 # Enable BQ API
-gcloud services enable bigquery.googleapis.com bigquerydatatransfer.googleapis.com --project=${PROJECT_ID}
+gcloud services enable \
+  bigquery.googleapis.com \
+  bigquerydatatransfer.googleapis.com \
+  apikeys.googleapis.com \
+  --project=${PROJECT_ID}
 
 # Create BQ Dataset
 bq --location=$LOCATION --project_id=${PROJECT_ID} mk --dataset --default_table_expiration 0 $DATASET_ID
@@ -17,8 +21,17 @@ bq --location=$LOCATION --project_id=${PROJECT_ID} mk --dataset --default_table_
 # https://cloud.google.com/bigquery-transfer/docs/reference/datatransfer/rest/v1/projects/enrollDataSources
 # Datasource to use: 6063d10f-0000-2c12-a706-f403045e6250
 
-API_KEY=`gcloud alpha services api-keys create --api-target=service=bigquerydatatransfer.googleapis.com`
+# Create API KEY
+gcloud alpha services api-keys create --api-target=service=bigquerydatatransfer.googleapis.com
 
+# Get UID of API KEY that we just created (sort by most recent, max 1)
+API_UID=`gcloud beta services api-keys list --sort-by=-createTime --limit=1 --format="value(uid)"`
+
+# Get Key String from API KEY
+API_KEY=`gcloud beta services api-keys get-key-string ${API_UID} --format="value(keyString)"`
+
+## ACTION REQUIRED ##
+## NEEDS OAUTH troubleshooting.... ##
 curl --request POST \
   'https://bigquerydatatransfer.googleapis.com/v1/projects/'${PROJECT_ID}':enrollDataSources?key='${API_KEY}'' \
   --header 'Authorization: Bearer ' $(gcloud auth print-identity-token) \
