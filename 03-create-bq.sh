5f83fec0131cd0ba1eac6f03d884174b83656c29
Jake Holmquist <101203373+cloud-jake@users.noreply.github.com>
2023-05-31 20:44:51 -0400

    Add token and auth for curl

--- 03-create-bq.sh
+++ 03-create-bq.sh
@@ -5,7 +5,7 @@ source variables.inc
 SA_FULL="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 
 # Enable BQ API
-gcloud services enable bigquery.googleapis.com --project=${PROJECT_ID}
+gcloud services enable bigquery.googleapis.com bigquerydatatransfer.googleapis.com --project=${PROJECT_ID}
 
 # Create BQ Dataset
 bq --location=$LOCATION --project_id=${PROJECT_ID} mk --dataset --default_table_expiration 0 $DATASET_ID
@@ -15,6 +15,15 @@ bq --location=$LOCATION --project_id=${PROJECT_ID} mk --dataset --default_table_
 # https://cloud.google.com/bigquery-transfer/docs/reference/datatransfer/rest/v1/projects/enrollDataSources
 # Datasource to use: 6063d10f-0000-2c12-a706-f403045e6250
 
+API_KEY=`gcloud alpha services api-keys create --api-target=service=bigquerydatatransfer.googleapis.com`
+
+curl --request POST \
+  'https://bigquerydatatransfer.googleapis.com/v1/projects/'${PROJECT_ID}':enrollDataSources?key='${API_KEY}'' \
+  --header 'Authorization: Bearer ' $(gcloud auth print-identity-token) \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data '{"dataSourceIds":["6063d10f-0000-2c12-a706-f403045e6250"]}' \
+  --compressed
 
 # Create Export
 
